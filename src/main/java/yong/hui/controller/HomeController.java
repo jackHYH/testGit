5b8497ce831b1ca7df87211a6f42e0d69dc0e802
jackHYH <923328916@qq.com>
2020-02-24 15:17:14 +0800

    第1次正式提交

---
+++ src/main/java/yong/hui/controller/HomeController.java
@@ -0,0 +1,19 @@
+package yong.hui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author hyh
+ * @version V1.0
+ * @Package yong.hui.controller
+ * @date 2020/2/24 14:57
+ * @Copyright © 2019.12.2
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
