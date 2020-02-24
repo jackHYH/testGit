6dce9ae76f21bc037ef274a71524ab17d10ed364
jackHYH <923328916@qq.com>
2020-02-24 15:23:09 +0800

    第二次正式提交

--- src/main/java/yong/hui/controller/HomeController.java
+++ src/main/java/yong/hui/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/home")
     public String home(){
+        int a =20;
         return "home";
     }
 }
